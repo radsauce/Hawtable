4958964990bb9d521f5556b27dac7709e01e1bdc
JoleenTsai <joleenjtsai@gmail.com>
2018-10-23 19:36:12 -0700

    reserve page

---
+++ views/jumbotron.jsx
@@ -0,0 +1,15 @@
+const React = require('react')
+const Fragment = React.Fragment
+
+const Jumbo = () => {
+  <Fragment>
+    <div class="jumbotron">
+      <h1 class="text-center"><span class="fa fa-fire"></span> Hot Restaurant</h1>
+      <hr />
+      <h2 class="text-center">We only have 5 tables! Book your seat before they all are gone!</h2>
+      <br />
+    </div>
+  </Fragment>
+}
+
+module.exports = Jumbo
